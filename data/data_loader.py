2678403df0eb8d75b8a892b1d3d111fbbf78299b
katar <cihan.katar@mevis.fraunhofer.de>
2026-02-25 15:10:09 +0300

    weight0.1_2-4

--- data/data_loader.py
+++ data/data_loader.py
@@ -48,12 +48,12 @@ def data_transform():
     return DinoMultiCropTransform(
         crop_transform_global, color_transform_global,
         crop_transform_local, color_transform_local,
-        n_global=2, n_local=8
+        n_global=2, n_local=4
     )
 class DinoMultiCropTransform:
     def __init__(self, crop_transform_global, color_transform_global,
                  crop_transform_local, color_transform_local,
-                 n_global=2, n_local=8):
+                 n_global=2, n_local=4):
         
         self.crop_global = crop_transform_global
         self.color_global = color_transform_global
